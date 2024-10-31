47614b7606d5ef788642df35219061b476f393b2
Edudominguez04 <edudominguez@correo.ugr.es>
2024-10-31 15:45:34 +0100

    Actualizado Creacion Almacen

--- seminario1/crear_almacen.sql
+++ seminario1/crear_almacen.sql
@@ -2,18 +2,18 @@ drop table if exists stock CASCADE;
 drop table if exists pedido CASCADE;
 drop table if exists detalle_pedido CASCADE;
 
-CREATE TABLE IF NOT EXISTS stock(
+CREATE TABLE stock(
     Cproducto INTEGER PRIMARY KEY,
     Cantidad INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS pedido(
+CREATE TABLE pedido(
     Cpedido INTEGER PRIMARY KEY,
     Ccliente INTEGER,
     Fecha_pedido DATE
 );
 
-CREATE TABLE IF NOT EXISTS detalle_pedido(
+CREATE TABLE detalle_pedido(
     Cpedido REFERENCES pedido(Cpedido),
     Cproducto REFERENCES stock(Cproducto),
     Cantidad INTEGER,
